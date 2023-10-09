057caacea8cd4415f3ff5b0796d498c392b8f121
ganded7006 <142960991+ganded7006@users.noreply.github.com>
2023-10-09 19:20:01 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 9.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+ * Updated on 10/9/23 DG
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request.
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
